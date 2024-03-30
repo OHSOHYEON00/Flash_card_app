edccbb74971d8d9552eefe125c4e3ba7a8ba21b3
OHSOHYEON00 <ohsohyeon24@gmail.com>
2024-03-30 20:52:36 +0100

    feat: setup react-testing-library, jest

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './index.css';
 export default function App() {
   return (
     <div className="bg-[#23262C] text-basic-white h-screen w-screen p-8 min-w-[325px] md:p-20 text-right">
-      asdfa
+      Hello world! I am using React
     </div>
   );
 }
