30612b06982e25448bcbf567dfe3875144b98b14
OHSOHYEON00 <ohsohyeon24@gmail.com>
2024-03-31 15:36:28 +0200

    fix: tailwind css setup - doesn't applied

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -6,14 +6,7 @@ const commonConfig = require('./webpack.config.js');
 const devConfig = merge(commonConfig, {
   mode: 'development',
   devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
+  module: {},
   devServer: {
     port: 8888,
     historyApiFallback: true,
