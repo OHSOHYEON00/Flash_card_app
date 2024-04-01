6d013ecc58add56b4ec023b97dcc32d6a9e8dd45
OHSOHYEON00 <ohsohyeon24@gmail.com>
2024-04-01 17:02:22 +0200

    feat: change directory structure (src -> app), layout greeting page

---
+++ app/Main/Projects/Projects.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Projects() {
+  return (
+    <section className="PROJECTS">
+      <h3>PROJECTS</h3>
+      <div>Explanation</div>
+    </section>
+  );
+}
+
+export default Projects;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{ts,tsx,html}'],
+  content: ['./app/**/*.{ts,tsx,html}'],
   theme: {
     extend: {
       colors: {
