c19580985bbe8b9da25a8f835de69bf966b19ba6
OHSOHYEON00 <ohsohyeon24@gmail.com>
2024-04-10 16:36:52 +0200

    feat: add next/font
    # Conflicts:
    #	app/components/SubHeader.tsx
    #	app/layout.tsx
    #	app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
+import { headerFont } from './layout';
 
 function Greeting() {
   return (
     <div className="h-full">
       <section className="h-full text-center flex flex-col justify-center items-center">
         <section className="">
-          <div className="text-5xl">Hey, I'm Sohyeon Oh</div>
-          <div className="text-4xl mt-2 mb-2">Front-end Developer</div>
-          <div className="mt-4">
-            As a dedicated learner and team player, I’m committed to pushing
-            technological boundaries and fostering an environment of continuous
-            improvement and collective achievement.
+          <div className={`text-5xl ${headerFont.className}`}>
+            Hey, I'm Sohyeon Oh
+          </div>
+          <div className={`text-4xl mt-2 mb-2 ${headerFont.className}`}>
+            Front-end Developer
           </div>
         </section>
       </section>
